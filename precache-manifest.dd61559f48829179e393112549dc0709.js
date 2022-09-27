3ec1d77f90e994b20ae3845fd99db2ee32b043f0
Emiliano Pérez <emi.esp86@gmail.com>
2022-09-27 16:37:57 -0300

    Updates

---
+++ precache-manifest.dd61559f48829179e393112549dc0709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434094de27386a3dc31600d05583431f",
+    "url": "/bReminder/index.html"
+  },
+  {
+    "revision": "a8b83daa0fe98c0a2705",
+    "url": "/bReminder/static/css/main.f1e15acc.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/bReminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bReminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b83daa0fe98c0a2705",
+    "url": "/bReminder/static/js/main.40d15026.chunk.js"
+  },
+  {
+    "revision": "18c282a1d1b48ee60cc8",
+    "url": "/bReminder/static/js/runtime-main.97b1acd0.js"
+  }
+]);
